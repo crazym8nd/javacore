2ea2fa77437fc30c846ec5de273a693c16297dd4
crazymind <no1me@bk.ru>
2023-09-12 20:12:33 +0300

    start of Chapter 5

---
+++ src/main/java/com/vitaly/javacore/chapter04/ByteShift.java
@@ -0,0 +1,12 @@
+package main.java.com.vitaly.javacore.chapter04;
+
+public class ByteShift {
+    public static void main(String[] args) {
+        byte a = 64, b;
+        int i;
+        i = a << 2;
+        b = (byte) (a <<2);
+        System.out.println("Первое число a: " + a);
+        System.out.println("i and b: " + i + " " + b);
+    }
+}
---
+++ src/main/java/com/vitaly/javacore/chapter04/Modilus.java
@@ -0,0 +1,10 @@
+package main.java.com.vitaly.javacore.chapter04;
+
+public class Modilus {
+    public static void main(String[] args) {
+        int x = 42;
+        double y = 42.25;
+        System.out.println("x mod = " + x % 10);
+        System.out.println("y mod = " + y % 10);
+    }
+}
